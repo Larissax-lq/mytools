5cce5bd1c3eac1abbacd967f68627be478b89fe5
Larissa Xu <lxu01@wesleyan.edu>
2024-10-15 15:29:34 -0400

    initial commit

---
+++ R/qscatter.R
@@ -0,0 +1,29 @@
+#'@title Quick scatter plot
+#'@description Scatter plot with line of best fit and correlation.
+#'@param data a dataframe
+#'@param x a numeric variable
+#'@param y a numeric variable
+#'@returns returns a ggplot2 graph
+#'@export
+#'@import ggplot2
+#'@examples
+#'qscatter(mtcars, wt, hp)
+
+
+qscatter <- function(data, x, y){
+  xname <- as.character(substitute(x))
+  yname <- as.character(substitute(y))
+  gtitle <- paste("Relationship between",
+                  xname, "and", yname)
+
+  r <- cor.test(data[[xname]], data[[yname]])
+  stitle <- paste("r =", round(r$estimate, 3),
+                  ", p<", format.pval(r$p.val,3))
+
+  ggplot(data, aes(x = {{x}}, y = {{y}})) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE, linetype = "dashed", color = "grey")+
+    theme_minimal()+
+    labs(title = gtitle,
+         subtitle = stitle)
+}
